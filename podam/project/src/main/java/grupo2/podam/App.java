dfe3583c94bcec1ff57c87649937f2803fe323b7
Juan Guzmán <jdguzmans@hotmail.com>
2018-09-28 22:16:13 -0500

    tried to install podam, unsuccessfully

---
+++ podam/project/src/main/java/grupo2/podam/App.java
@@ -0,0 +1,16 @@
+package grupo2.podam;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+	PodamFactory factory = new PodamFactoryImpl();
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
